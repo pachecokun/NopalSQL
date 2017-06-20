bf3c708ce440bd99aaea9bedbd8eb082fbf54fae
Saul Trujillo <strujillog1401@alumno.ipn.mx>
2017-06-20 01:55:43 -0500

    asdfg

---
+++ NN/graphWeight.m
@@ -0,0 +1,27 @@
+function graphWeight(cells, numCapa)    
+    hold on;
+    grid on;
+    numMatrices = size(cells,1);
+    [S R] = size(cell2mat(cells(1)));
+    j = 1;   
+    totalW = S*R;    
+    legendsW = cell(1,totalW);
+    for c = 1: totalW        
+        legendsW{j} = strcat('W',int2str(c)); 
+        j = j+1;    
+    end          
+    for c = 1:totalW
+        vector = zeros(1,numMatrices);
+        for i = 1:numMatrices            
+            matriz = cell2mat(cells(i)); 
+            vector(1,i)=matriz(c);                                  
+        end 
+        plot(vector);       
+    end   
+    hold off;
+    title(strcat('Evolucion de pesos de capa ',numCapa));
+    xlabel('k');
+    ylabel('W');    
+    legend(legendsW);
+    disp(counter);
+end
